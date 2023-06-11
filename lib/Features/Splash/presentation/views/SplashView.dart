b00ec60a912543f5b8cd65e90e2abd25bdb07b68
Hamdy248 <135514777+Hamdy248@users.noreply.github.com>
2023-06-11 21:44:29 +0300

    create splash view body

--- lib/Features/Splash/presentation/views/SplashView.dart
+++ lib/Features/Splash/presentation/views/SplashView.dart
@@ -9,7 +9,6 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      backgroundColor: Colors.black,
       body: SplashViewBody(),
     );
   }
