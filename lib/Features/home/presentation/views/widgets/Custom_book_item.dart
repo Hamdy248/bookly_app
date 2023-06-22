dea208279bb6824adae1fa03727a43854511a330
Hamdy248 <135514777+Hamdy248@users.noreply.github.com>
2023-06-22 14:42:24 +0300

    add text to book details

--- lib/Features/home/presentation/views/widgets/Custom_book_item.dart
+++ lib/Features/home/presentation/views/widgets/Custom_book_item.dart
@@ -10,7 +10,7 @@ class CustomBookItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.5 / 4,
+      aspectRatio: 2.7 / 4,
       child: Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
--- lib/Features/home/presentation/views/widgets/book_detalis_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_detalis_view_body.dart
@@ -1,14 +1,16 @@
 // ignore_for_file: camel_case_types, unused_import, unused_local_variable
 
 import 'package:bookly_app/Features/home/presentation/views/widgets/Custom_book_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/widgets/FeaturedlistViewItem.dart';
 import 'package:bookly_app/core/utils/asset.dart';
+import 'package:bookly_app/core/utils/custom_style.dart';
 import 'package:flutter/material.dart';
 
 import 'widgets/customBookDetalisViewAppbar.dart';
 
 class BookDetalisViewBody extends StatelessWidget {
   const BookDetalisViewBody({super.key});
-
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
@@ -18,9 +20,33 @@ class BookDetalisViewBody extends StatelessWidget {
         children: [
           const customBookDetalisViewAppbar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            padding: EdgeInsets.symmetric(horizontal: width * .15),
             child: const CustomBookItem(),
-          )
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: style.TextStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          const Opacity(
+            opacity: 0.7,
+            child: Text(
+              'Rudyard Kipling',
+              style: style.TextStyle18,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 120),
+            child: BookRating(),
+          ),
         ],
       ),
     );
